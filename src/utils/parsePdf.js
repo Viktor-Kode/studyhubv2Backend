528aaad4e6043f384a37e84de70a677fa40e885d
viktor <victorand804@gmail.com>
2026-05-05 21:41:25 +0100

    fix: use more robust DOMMatrix and DOMRect polyfills for PDF parsing

--- src/utils/parsePdf.js
+++ src/utils/parsePdf.js
@@ -2,7 +2,23 @@ import { createRequire } from 'module';
 
 // Add this before your pdf parsing code 
 if (typeof globalThis.DOMMatrix === 'undefined') {
-  globalThis.DOMMatrix = class DOMMatrix {};
+  globalThis.DOMMatrix = class DOMMatrix {
+    constructor() {
+      this.a = 1; this.b = 0;
+      this.c = 0; this.d = 1;
+      this.e = 0; this.f = 0;
+    }
+  };
+}
+
+if (typeof globalThis.DOMRect === 'undefined') {
+  globalThis.DOMRect = class DOMRect {
+    constructor(x = 0, y = 0, width = 0, height = 0) {
+      this.x = x; this.y = y;
+      this.width = width;
+      this.height = height;
+    }
+  };
 }
 
 const require = createRequire(import.meta.url);
